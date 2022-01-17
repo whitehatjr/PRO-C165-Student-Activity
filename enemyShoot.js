4c167e0cab1e6f89e8bf82c43d6dbfdfd88d40e5
PreetiSharma15 <pwhitehat6@gmail.com>
2022-01-17 16:47:48 +0530

    Boilerplate Code Updated

---
+++ enemyShoot.js
@@ -0,0 +1,61 @@
+AFRAME.registerComponent("enemy-bullets", {
+    init: function () {
+        setInterval(this.shootEnemyBullet, 2000)
+    },
+    shootEnemyBullet: function () {
+
+        //get all enemies using className
+        var els = document.querySelectorAll(".enemy");
+
+        for (var i = 0; i < els.length; i++) {
+
+            //enemyBullet entity
+            var enemyBullet = document.createElement("a-entity");
+
+            enemyBullet.setAttribute("geometry", {
+                primitive: "sphere",
+                radius: 0.1,
+            });
+
+            enemyBullet.setAttribute("material", "color", "#282B29");
+
+            var position = els[i].getAttribute("position")
+
+            enemyBullet.setAttribute("position", {
+                x: position.x + 1.5,
+                y: position.y + 3.5,
+                z: position.z,
+            });
+
+            var scene = document.querySelector("#scene");
+            scene.appendChild(enemyBullet);
+
+            //Three.js Vector Variables
+            
+
+            //Get enemey and player position using Three.js methods
+            
+
+            //set the velocity and it's direction
+            
+            //Set dynamic-body attribute
+            
+
+            //Get text attribute
+            
+
+            //collide event on enemy bullets
+            enemyBullet.addEventListener("collide", function (e) {
+                if (e.detail.body.el.id === "weapon") {
+
+                    //Add the conditions here
+
+
+
+                }
+            });
+
+        }
+    },
+
+});
